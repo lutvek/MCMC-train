9bc1d5fe1ae3a90741381fe40a8cf4b69bfa8d0b
Henrik Westerberg <henke@n146-p111.kthopen.kth.se>
2017-01-09 14:03:18 +0100

    graph.py created

---
+++ graph.py
@@ -0,0 +1,20 @@
+import numpy as np
+import random, time, math, itertools
+from collections import OrderedDict
+import matplotlib.pyplot as plt
+
+
+class Vertex:
+
+	def __init__(self):
+		self.L, self.R, self.O = None
+
+
+class Graph:
+
+	def generate_vertexes(self):
+
+
+	def connect_vertexes(self):
+
+	def 
